96f9987d5fcced347909add0afb907c30ba30d75
Evandro-Meurer <evandrofriedrichmeurer237@gmail.com>
2026-04-10 01:58:20 -0300

    Feito o MatriculaRepository

--- src/main/java/org/example/repository/MatriculaRepository.java
+++ src/main/java/org/example/repository/MatriculaRepository.java
@@ -1,4 +1,36 @@
 package org.example.repository;
 
+import jakarta.persistence.EntityManager;
+import org.example.entity.MatriculaEntity;
+import java.util.List;
+import java.util.Optional;
+
 public class MatriculaRepository {
+
+    private final EntityManager entityManager;
+
+    public MatriculaRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public MatriculaEntity salvar(MatriculaEntity matricula) {
+        if (matricula.getId() == null) {
+            entityManager.persist(matricula);
+            return matricula;
+        } else {
+            return entityManager.merge(matricula);
+        }
+    }
+
+    public Optional<MatriculaEntity> buscarPorId(Long id) {
+        return Optional.ofNullable(entityManager.find(MatriculaEntity.class, id));
+    }
+
+    public List<MatriculaEntity> listarTodas() {
+        return entityManager
+                .createQuery("SELECT m FROM MatriculaEntity m", MatriculaEntity.class)
+                .getResultList();
+    }
+
+    public void deletar(MatriculaEntity matricula) { entityManager.remove(matricula);}
 }
