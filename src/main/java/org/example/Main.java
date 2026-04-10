219c8b0579415988ba91e5b33468d38a1639659c
Keetta <nicholasfelixloko2@gmail.com>
2026-04-10 16:20:35 -0300

    Finalmente colocamos tudo do projeto na Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,72 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import jakarta.persistence.*;
+import org.example.entity.*;
+import org.example.service.*;
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("meuPU");
+        EntityManager em = emf.createEntityManager();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Todos os Services
+        AlunoService alunoService = new AlunoService(em);
+        PlanoService planoService = new PlanoService(em);
+        MatriculaService matriculaService = new MatriculaService(em);
+        PagamentoService pagamentoService = new PagamentoService(em);
+        TreinadorService treinadorService = new TreinadorService(em);
+        TreinoService treinoService = new TreinoService(em);
+        try {
+            // Cria Plano
+            PlanoEntity plano = new PlanoEntity();
+            plano.setNome("BASICO");
+            plano.setDescricao("Plano básico mensal");
+            plano = planoService.criarPlano(plano);
+
+            // Cria Aluno + Endereço
+            EnderecoEntity endereco = new EnderecoEntity();
+            endereco.setRua("Rua A");
+            endereco.setNumero("123");
+            endereco.setCidade("São Paulo");
+
+            AlunoEntity aluno = new AlunoEntity();
+            aluno.setNome("Nick");
+            aluno.setIdade(20);
+            aluno.setTelefone("11999999999");
+            aluno.setAltura(new BigDecimal("1.75"));
+            aluno.setPeso(new BigDecimal("70.00"));
+            aluno.setEndereco(endereco);
+            aluno = alunoService.criarAluno(aluno);
+
+            // Matricula o Aluno
+            MatriculaEntity matricula = matriculaService
+                    .matricularAluno(aluno.getId(), plano.getId());
+            // Cria o Pagamento
+            PagamentoEntity pagamento = pagamentoService
+                    .criarPagamento(matricula.getId(), new BigDecimal("140.00"));
+            // Registrando Pagamento
+            pagamentoService.registrarPagamento(pagamento.getId());
+
+            // Cria o Treinador
+            TreinadorEntity treinador = new TreinadorEntity();
+            treinador.setNome("Carlos");
+            treinador.setEspecialidade("Hipertrofia");
+            treinador.setTelefone("11888888888");
+            treinador = treinadorService.criarTreinador(treinador);
+
+            // Cria o Treino
+            TreinoEntity treino = new TreinoEntity();
+            treino.setNome("Treino A");
+            treino.setDescricao("Peito e tríceps");
+            treinoService.criarTreino(treino, treinador.getId());
+
+            System.out.println("✅ Sistema executado com sucesso!");
+        } catch (Exception e) {
+            System.out.println("❌ Erro: " + e.getMessage());
+        } finally {
+            em.close();
+            emf.close();
         }
     }
 }
